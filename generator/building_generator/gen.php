8da9497c449b7a96c67ef8157ed2568e493bf8f1
Daniel Jakob <wolverine@usox.org>
2019-10-19 10:49:19 +0200

    Add building generator

---
+++ generator/building_generator/gen.php
@@ -0,0 +1,126 @@
+<?php
+
+$basePath = sprintf(
+    '%s/../buildings/',
+    __DIR__
+);
+
+$destinationPath = sprintf(
+    '%s/../../generated/buildings/',
+    __DIR__
+);
+
+$imageWidth = 40;
+$imageHeight = 40;
+
+$fileNames = [
+    '0an',
+    '0at',
+    '0bn',
+    '0bt'
+];
+
+$dirContent = glob(
+    sprintf(
+        '%s/*',
+        $basePath
+    ),
+    GLOB_ONLYDIR
+);
+
+foreach ($dirContent as $dir) {
+
+    $configFilePath = sprintf(
+        '%s/config.json',
+        $dir
+    );
+
+    if (!file_exists($configFilePath)) {
+        continue;
+    }
+    $config = json_decode(file_get_contents($configFilePath));
+
+    $baseBuildingId = $config->base_building_id;
+
+    $buildingSourcePath = sprintf(
+        '%s/%d',
+        $basePath,
+        $baseBuildingId
+    );
+
+    $buildingDestinationPath = sprintf(
+        '%s/%d',
+        $destinationPath,
+        $baseBuildingId
+    );
+
+    @mkdir(
+        $buildingDestinationPath,
+        0755,
+        true
+    );
+
+    foreach ($fileNames as $fileName) {
+        copy(
+            sprintf(
+                '%s/%s.png',
+                $buildingSourcePath,
+                $fileName
+            ),
+            sprintf(
+                '%s/%s.png',
+                $buildingDestinationPath,
+                $fileName
+            )
+        );
+    }
+
+    foreach ($config->buildable_fields as $item) {
+        if ($item->bonus_type === 0) {
+            continue;
+        }
+
+        $buildingDestinationPath = sprintf(
+            '%s/%d',
+            $destinationPath,
+            $item->building_id
+        );
+
+        @mkdir(
+            $buildingDestinationPath,
+            0755,
+            true
+        );
+
+        foreach ($fileNames as $fileName) {
+            $baseImagePath = sprintf(
+                '%s/%s.png',
+                $buildingSourcePath,
+                $fileName
+            );
+
+            $fragmentImagePath = sprintf(
+                '%s/fragment/bonus%d.png',
+                __DIR__,
+                $item->bonus_type
+            );
+
+            $destinationImage = imagecreatetruecolor($imageWidth, $imageHeight);
+            imagesavealpha($destinationImage, true);
+            imagealphablending($destinationImage, false);
+
+            $baseImage = imagecreatefrompng($baseImagePath);
+            $fragmentImage = imagecreatefrompng($fragmentImagePath);
+
+            imagecopy($destinationImage, $baseImage, 0 ,0, 0, 0, $imageWidth, $imageHeight);
+            imagecopy($destinationImage, $fragmentImage, 24 ,24, 0, 0, $imageWidth, $imageHeight);
+            imagepng(
+                $destinationImage,
+                sprintf('%s/%s.png', $buildingDestinationPath, $fileName)
+            );
+
+            imagedestroy($destinationImage);
+            imagedestroy($baseImage);
+        }
+    }
+}
