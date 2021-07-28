f1b013540786ba02fbcc157c438cbc8c68d84028
Huxinator <52821266+Huxinator@users.noreply.github.com>
2021-07-28 21:26:31 +0200

    Revert "pngupdate (#612)"
    This reverts commit d52557d323118e4c32c4345a89f149acc56c6cb5.

--- löschen.bat
+++
@@ -1,9 +0,0 @@
-echo Loeschung der Dateien beginnt...
-del C:\Users\CC\Desktop\assets-master\assets-master\*.gif /f /q /s
-del C:\Users\CC\Desktop\assets-master\assets-master\*.psd /f /q /s
-del C:\Users\CC\Desktop\assets-master\assets-master\*.txt /f /q /s
-del C:\Users\CC\Desktop\assets-master\assets-master\*.docx /f /q /s
-del C:\Users\CC\Desktop\assets-master\assets-master\*.md /f /q /s
-del C:\Users\CC\Desktop\assets-master\assets-master\*.jpg /f /q /s
-echo Loeschung abgeschlossen...
-end
